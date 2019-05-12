8b15393f4e5e221c1325b568ec67f9b020272331
Neji Argoubi <argoubinaji@hotmail.fr>
2019-05-12 03:20:13 +0200

    Add comments

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -4,6 +4,7 @@ package hello;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Message message = new Message();
+		//add comment:Say Hello Test
 		System.out.println(message.sayHello());
 	}
 }
